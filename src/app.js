281abfd986c48e65cf4e8ec1d9c0059ccce8cf7a
crimson1023 <c258789456@gmail.com>
2024-10-01 12:49:05 -0700

    feat(path): rename to lowercase

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import MentorPage from "./pages/mentorPage";
+import LearnerPage from "./pages/learnerPage";
+import StatisticsPage from "./pages/statisticsPage";
+import HomePage from "./pages/homePage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/mentor" element={<MentorPage />} />
+        <Route path="/learner" element={<LearnerPage />} />
+        <Route path="/statistic" element={<StatisticsPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
