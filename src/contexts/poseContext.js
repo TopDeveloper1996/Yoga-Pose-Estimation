e0bbac28a17049b2dc72c227147de0c422abe876
crimson1023 <c258789456@gmail.com>
2024-10-02 10:04:23 -0700

    feat(context): create pose context

---
+++ src/contexts/poseContext.js
@@ -0,0 +1,157 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+
+const PoseContext = createContext({
+  poses: [],
+  loadingPose: false,
+  errorPose: null,
+  addPose: (poseData, images = [], video = null) => {},
+  deletePose: ({ pose_detection_id, pose_id }) => {},
+  getPose: ({
+    pose_id,
+    pose_detection_id,
+    campus_group_id,
+    c_subject_id,
+    chapter_id,
+    topic_id,
+    pose_name,
+    sts,
+  }) => {},
+  updatePose: (poseData, images = [], video = null) => {},
+});
+
+const PoseProvider = ({ children }) => {
+  const [loadingPose, setLoadingPose] = useState(false);
+  const [errorPose, setErrorPose] = useState(null);
+
+  const AsyncCatchError =
+    (fn) =>
+    async (...args) => {
+      setLoadingPose(true);
+      setErrorPose(null);
+      try {
+        await fn(...args);
+      } catch (err) {
+        setErrorPose(err.message || "An error occurred");
+      } finally {
+        setLoadingPose(false);
+      }
+    };
+
+  const addPose = AsyncCatchError(
+    async (poseData, images = [], video = null) => {
+      const formData = new FormData();
+
+      formData.append("data", JSON.stringify(poseData));
+
+      if (images && images.length > 0) {
+        images.forEach((image, index) => {
+          formData.append(`pose_images`, image);
+        });
+      }
+
+      if (video) {
+        formData.append("pose_video", video);
+      }
+
+      const response = await axios.post(
+        "api/campus_dashboard_module_ms/yogaPoses/addPoseDetections",
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
+
+      if (response.status === "201") {
+      } else {
+        throw new Error(response.data.message || "Failed to add pose");
+      }
+    }
+  );
+
+  const getPose = AsyncCatchError(
+    async ({
+      pose_id,
+      pose_detection_id,
+      campus_group_id,
+      c_subject_id,
+      chapter_id,
+      topic_id,
+      pose_name,
+      sts,
+    }) => {
+      const response = await axios.post(
+        "/api/campus_dashboard_module_ms/yogaPoses/getPoseDetections",
+        {
+          pose_id,
+          pose_detection_id,
+          campus_group_id,
+          c_subject_id,
+          chapter_id,
+          topic_id,
+          pose_name,
+          sts,
+        }
+      );
+
+      if (response.status === "200") {
+      } else {
+        throw new Error(response.data.message || "Failed to get pose");
+      }
+    }
+  );
+
+  const updatePose = AsyncCatchError(
+    async ({ poseData, images = [], video = null }) => {
+      const formData = new FormData();
+
+      formData.append("data", JSON.stringify(poseData));
+
+      if (images && images.length > 0) {
+        images.forEach((image, index) => {
+          formData.append(`pose_images`, image);
+        });
+      }
+
+      if (video) {
+        formData.append("pose_video", video);
+      }
+
+      const response = await axios.post(
+        "api/campus_dashboard_module_ms/yogaPoses/addPoseDetections",
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
+
+      if (response.status === "200") {
+      } else {
+        throw new Error(response.data.message || "Failed to add pose");
+      }
+    }
+  );
+
+  const deletePose = AsyncCatchError(async ({ pose_detection_id, pose_id }) => {
+    const response = await axios.delete(
+      "/api/campus_dashboard_module_ms/yogaPoses/deletePoseDetections",
+      {
+        params: {
+          pose_detection_id,
+          pose_id,
+        },
+      }
+    );
+
+    if (response.status === "204") {
+    } else {
+      throw new Error(response.data.message || "Failed to add pose");
+    }
+  });
+
+  return (
+    <PoseContext.Provider
+      value={(loadingPose, errorPose, addPose, getPose, deletePose, updatePose)}
+    >
+      {children}
+    </PoseContext.Provider>
+  );
+};
+
+export { PoseContext, PoseProvider };
