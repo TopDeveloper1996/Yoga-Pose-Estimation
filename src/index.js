f5b8f417474dda380ec62e182f9d1b6ffd165a74
crimson1023 <c258789456@gmail.com>
2024-10-02 10:10:19 -0700

    feat: pack app with mentor and pose provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app";
+import reportWebVitals from "./reportWebVitals";
+import { MentorProvider } from "./contexts/mentorContext";
+import { PoseProvider } from "./contexts/poseContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <PoseProvider>
+      <MentorProvider>
+        <App />
+      </MentorProvider>
+    </PoseProvider>
   </React.StrictMode>
 );
 
